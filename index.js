d4176b2f39b90cc7aa71b2280f473c6d080957f8
kausarahamed <galibhasan517@gmail.com>
2022-08-03 16:28:03 +0600

    kausar

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("welcome to Knowledge Zone.......");
+  res.send("welcome to Knowledge Zone..");
 });
 
 app.listen(port, () => {
