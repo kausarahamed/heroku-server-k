988cb38b5e8df9eb04f35d39d727de73dddccd29
kausarahamed <galibhasan517@gmail.com>
2022-08-04 17:24:35 +0600

    kausar

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJwt(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbbiden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
@@ -27,7 +42,28 @@ async function run() {
       .db("knowledge-zone")
       .collection("blog-collection");
 
+    // for user collection (faisal)
+
+    const userCollection = client.db("knowledge-zone").collection("users");
+
     // for  class one to twelve database start
+
+    // for courses routes  start
+
+    const freeCourse = client.db("courses").collection("freeCourse");
+    const discountCourse = client.db("courses").collection("discountCourse");
+    const liveCourse = client.db("courses").collection("liveCourse");
+    const specialCourse = client.db("courses").collection("specialCourse");
+    const islamicCourse = client.db("courses").collection("islamicCourse");
+    const kidsCourse = client.db("courses").collection("kidsCourse");
+    const entertainCourse = client.db("courses").collection("entertainCourse");
+
+    // for courses routes  start
+
+    app.get("/books", async (req, res) => {
+      const result = await booksCollection.find().toArray();
+      res.send(result);
+    });
 
     const ClassOneCourse = client.db("classOneToTwelve").collection("classOne");
 
@@ -76,6 +112,55 @@ async function run() {
     app.get("/blogs", async (req, res) => {
       const result = await blogCollection.find().toArray();
       res.send(result);
+    });
+
+    // for user collection (faisal)
+
+    app.get("/user", verifyJwt, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.put("/user/admin/:email", verifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden message" });
+      }
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "2d" }
+      );
+      res.send({ result, token });
     });
 
     // for  class one to twelve start
@@ -131,7 +216,39 @@ async function run() {
     });
 
     // for  class one to twelve end
-    // class one to twelve student api donegi
+    // class one to twelve student api done
+
+    // for course routes api create  start
+    app.get("/freeCourse", async (req, res) => {
+      const result = await freeCourse.find().toArray();
+      res.send(result);
+    });
+    app.get("/discountCourse", async (req, res) => {
+      const result = await discountCourse.find().toArray();
+      res.send(result);
+    });
+    app.get("/freeCourse", async (req, res) => {
+      const result = await liveCourse.find().toArray();
+      res.send(result);
+    });
+    app.get("/specialCourse", async (req, res) => {
+      const result = await specialCourse.find().toArray();
+      res.send(result);
+    });
+    app.get("/islamicCourse", async (req, res) => {
+      const result = await islamicCourse.find().toArray();
+      res.send(result);
+    });
+    app.get("/kidsCourse", async (req, res) => {
+      const result = await kidsCourse.find().toArray();
+      res.send(result);
+    });
+    app.get("/entertainCourse", async (req, res) => {
+      const result = await entertainCourse.find().toArray();
+      res.send(result);
+    });
+    // for course routes api create  end
+    // done
   } finally {
     //   await client.close();
   }
@@ -139,7 +256,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("welcome to Knowledge Zone..");
+  res.send("welcome to Knowledge Zone.......");
 });
 
 app.listen(port, () => {
